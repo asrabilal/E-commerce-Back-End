1e0b8b4a428a6228f1040549b51f0fd09b0c2c22
AsraBilal <107981178+asrabilal@users.noreply.github.com>
2022-10-01 10:05:30 -0400

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,9 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
+      host: 'localhost',
       dialect: 'mysql',
+      port:3305,
       dialectOptions: {
         decimalNumbers: true,
       },
